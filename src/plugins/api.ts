cc419641db9439bcffa2a92d163e34bff8ea76d7
whitedragon10293 <whitedragon10293@gmail.com>
2021-09-24 06:35:21 +0200

    remove heroku

--- src/plugins/api.ts
+++ src/plugins/api.ts
@@ -5,7 +5,7 @@ const VERSION = '1.1.0'
 
 const apiPlugin: Plugin = (ctx, inject) => {
   const api: NuxtApiInstance = {
-    getPrices: () => ctx.$axios.get('https://crprice-api.herokuapp.com/prices'),
+    getPrices: () => ctx.$axios.get('https://api.cropper.finance/prices/?t='+ Math.round((new Date().getTime()) / 60000) ),
     getEpochInfo: (rpc: string) => ctx.$axios.post(rpc, { jsonrpc: '2.0', id: 1, method: 'getEpochInfo' })
   }
 
